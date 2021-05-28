84bbbc6a7e2b955728a1b8e3794038e2eb335880
ACuteWoof <sutharshanvithushan@gmail.com>
2021-05-28 23:25:42 +0530

    temp

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,47 @@
-function displayCircledList(){
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function displayBlock(block){
+  block.style.opacity = 1;
+}
+
+function hideBlock(block){
+  block.style.opacity = 1;
+}
+
+async function displayCircledList(){
+  displayBlock(document.getElementById("tl"));
+  await sleep(300);
+  displayBlock(document.getElementById("tr"));
+  await sleep(300);
+  displayBlock(document.getElementById("br"));
+  await sleep(300);
+  displayBlock(document.getElementById("bl"));
+}
+
+async function hideCircledList(){
+  hideBlock(document.getElementById("bl"));
+  await sleep(300);
+  hideBlock(document.getElementById("br"));
+  await sleep(300);
+  hideBlock(document.getElementById("tr"));
+  await sleep(300);
+  hideBlock(document.getElementById("tl"));
+}
+
+async function displayGithub(){
+  
+}
+
+async function displayYoutube(){
+
+}
+
+async function displayDiscord(){
+
+}
+
+async function displayTopProject(){
 
 }
