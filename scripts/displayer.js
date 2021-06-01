bd91de27856cbcb4f42b8823eff92cc57a9dc00c
ACuteWoof <sutharshanvithushan@gmail.com>
2021-06-01 21:42:55 +0530

    FIRST RELEASE FOR REMAKE

--- scripts/displayer.js
+++ scripts/displayer.js
@@ -8,44 +8,38 @@ var user_inf = null;
 
 const xhr = new XMLHttpRequest();
 
-async function displayGithub(){
-  yt.style.opacity = 0;
-  await sleep(300);
-  yt.style.display = 'none';
-  await  sleep(300);
-  ds.style.opacity = 0;
-  await sleep(300);
-  ds.style.display = 'none';
-  await sleep(300);
-  tp.style.opacity = 0;
-  await sleep(300);
-  tp.style.display = 'none';
 
-  root.onmouseover = '';
+async function displayGithub(){
 
   xhr.open('GET', 'https://api.github.com/users/acutewoof', true);
 
   xhr.onload = async function() {
     json = JSON.parse(this.response)
 
+    sleepTime = 3000;
+
+    root.innerHTML = ''
+    await sleep(100);
+
     root.innerHTML =`<h1 id='name'>${json['name']}</h1>`;
-    await sleep(4000);
+    await sleep(2000);
 
-    root.innerHTML = `<h4 id='repo'>Public Repos: ${String(json['public_repos']).trim()}</h4>`;
-    await sleep(4000);
+    root.innerHTML = `<h4 id='repo'>Public Repos: ${String(json['public_repos'])}</h4>`;
+    await sleep(sleepTime);
 
-    root.innerHTML = `<h4 id='loc'>Location: ${String(json['location']).trim()}</h4>`;
-    await sleep(4000);
+    root.innerHTML = `<h4 id='loc'>Location: ${String(json['location'])}</h4>`;
+    await sleep(sleepTime);
 
-    root.innerHTML = `<h4 id='followers'>Followers: ${String(json['followers']).trim()}</h4>`
-    await sleep(4000);
+    root.innerHTML = `<h4 id='followers'>Followers: ${String(json['followers'])}</h4>`
+    await sleep(sleepTime);
 
-    root.innerHTML = `<img class='pfp' id='img' src="https://avatars.githubusercontent.com/acutewoof" onclick='window.open("https://github.com/acutewoof")' onmouseover='displayCircledList()'>`
+    root.innerHTML = `<h4 id='following'>Following: ${String(json['following'])}</h4>`
+    await sleep(sleepTime);
+
+    root.innerHTML = `<img class='pfp' id='img' src="https://avatars.githubusercontent.com/acutewoof" onclick='window.open("https://github.com/acutewoof")' onmouseover='displayGithub()'>`
     document.getElementById('img').style.opacity = 0;
     await sleep(300);
     document.getElementById('img').style.opacity = 1;
-    
-    displayCircledList()
 
   }
 
@@ -53,53 +47,7 @@ async function displayGithub(){
 
 }
 
-async function displayYoutube(){
-  gh.style.opacity = 0;
-  await sleep(300);
-  gh.style.display = 'none';
-  await sleep(300);
-  ds.style.opacity = 0;
-  await sleep(300);
-  ds.style.opacity = 'none';
-  await sleep(300);
-  tp.style.opacity = 0;
-  await sleep(300);
-  tp.style.display = 'none';
-
-  root.onmouseover = '';
-
-}
-
-async function displayDiscord(){
-  yt.style.opacity = 0;
-  await sleep(300);
-  yt.style.display = 'none';
-  await  sleep(300);
-  gh.style.opacity = 0;
-  await sleep(300);
-  gh.style.display = 'none';
-  await sleep(300);
-  tp.style.opacity = 0;
-  await sleep(300);
-  tp.style.display = 'none';
-
-  root.onmouseover = '';
-  
-} 
 
 async function displayTopProject(){
-  yt.style.opacity = 0;
-  await sleep(300);
-  yt.style.display = 'none';
-  await  sleep(300);
-  gh.style.opacity = 0;
-  await sleep(300);
-  gh.style.display = 'none';
-  await sleep(300);
-  ds.style.opacity = 0;
-  await sleep(300);
-  ds.style.display = 'none';
-
-  root.onmouseover = '';
-
+  root.onmouseover = displayTopProject();
 }
--- scripts/index.js
+++ scripts/index.js
@@ -13,23 +13,3 @@ async function hideBlock(block){
   await sleep(500)
   block.style.display = 'none';
 }
-
-async function displayCircledList(){
-  displayBlock(document.getElementById("tl"));
-  await sleep(300);
-  displayBlock(document.getElementById("tr"));
-  await sleep(300);
-  displayBlock(document.getElementById("br"));
-  await sleep(300);
-  displayBlock(document.getElementById("bl"));
-}
-
-async function hideCircledList(){
-  hideBlock(document.getElementById("bl"));
-  await sleep(300);
-  hideBlock(document.getElementById("br"));
-  await sleep(300);
-  hideBlock(document.getElementById("tr"));
-  await sleep(300);
-  hideBlock(document.getElementById("tl"));
-}
