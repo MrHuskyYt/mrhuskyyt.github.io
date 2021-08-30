6cdd5b151df439789235374609c22e1aa68abb74
ACuteWoof <sutharshanvithushan@gmail.com>
2021-08-30 20:33:15 +0530

    remake - blobbbb

--- scripts/repos.js
+++ scripts/repos.js
@@ -1,22 +1,4 @@
-const reposUL = document.getElementById("repos");
 const projectsUL = document.getElementById("projects");
-
-function getAllRepos(mainUL) {
-  const xhr = new XMLHttpRequest();
-  const url = `https://api.github.com/users/acutewoof/repos`;
-  xhr.open("GET", url, true);
-
-  xhr.onload = function () {
-    const data = JSON.parse(this.response);
-    for (let i in data) {
-      const name = data[i].name;
-      const url = data[i].url;
-      mainUL.innerHTML =
-        mainUL.innerHTML + `<li><a href="${url}">${name}</a></li>`;
-    }
-  };
-  xhr.send();
-}
 
 function getAllProjects(mainUL) {
   const xhr = new XMLHttpRequest();
@@ -30,11 +12,17 @@ function getAllProjects(mainUL) {
       const name = data[i].name;
       const url = data[i].url;
       mainUL.innerHTML =
-        mainUL.innerHTML + `<li><a href="${url}">${name}</a></li>`;
+        mainUL.innerHTML +
+        `<div class="blob-wrapper"><div class="blob blob${i}" onclick="redirect('${url}')"><h3>${name}</h3></div></div>`;
+
+      generateBlob(`.blob${i}`);
     }
   };
   xhr.send();
 }
 
+function redirect(url) {
+  window.location.href = url;
+}
+
 getAllProjects(projectsUL);
-getAllRepos(reposUL);
